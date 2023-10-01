d868396a95d56a56c115b6ebb35722d75c98d429
Andrew Theis <andrewtheis@icloud.com>
2023-10-01 18:59:43 -0500

    Add helpers for URL manipulation

---
+++ Sources/SecondFoundation/Extensions/URL+SecondFoundation.swift
@@ -0,0 +1,24 @@
+//
+//  Copyright © 2023 Hidden Spectrum, LLC. All rights reserved.
+//
+
+import Foundation
+import UniformTypeIdentifiers
+
+
+public extension URL {
+
+    /// Appends the filename from another URL replacing it's extension with the given `UTType`.
+    /// - Parameter utType: `UTType` of the file extension to use.
+    /// - Returns: Completed `URL`.
+    func appendingFileName(from otherUrl: URL, conformingTo utType: UTType) -> URL {
+        return appendingPathComponent(otherUrl.deletingPathExtension().lastPathComponent, conformingTo: utType)
+    }
+    
+    /// Replaces file extension from given `UTType`.
+    /// - Parameter utType:`UTType` of the file extension to use.
+    /// - Returns: Updated `URL`.
+    func replacingFileExtension(with utType: UTType) -> URL {
+        return deletingPathExtension().appendingPathExtension(for: utType)
+    }
+}
---
+++ Tests/SecondFoundationTests/Extensions/URL+SecondFoundationTests.swift
@@ -0,0 +1,30 @@
+//
+//  Copyright © 2023 Hidden Spectrum, LLC. All rights reserved.
+//
+
+@testable import SecondFoundation
+import UniformTypeIdentifiers
+import XCTest
+
+
+final class URL_SecondFoundationTests: XCTestCase {
+    
+    func testAppendingFileName() {
+        let originalURL = URL(string: "https://example.com/original/")!
+        let anotherURL = URL(string: "https://example.com/sample.txt")!
+        let utType = UTType("public.jpeg")!
+        
+        let resultURL = originalURL.appendingFileName(from: anotherURL, conformingTo: utType)
+        
+        XCTAssertEqual(resultURL, URL(string: "https://example.com/original/sample.jpeg")!)
+    }
+    
+    func testReplacingFileExtension() {
+        let originalURL = URL(string: "https://example.com/original.mp4")!
+        let utType = UTType("public.jpeg")!
+        
+        let resultURL = originalURL.replacingFileExtension(with: utType)
+        
+        XCTAssertEqual(resultURL, URL(string: "https://example.com/original.jpeg")!)
+    }
+}
