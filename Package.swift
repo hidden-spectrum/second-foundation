7f422086f1473ac2711c7d1084b7c23f594e63bc
Andrew Theis <andrew@hiddenspectrum.io>
2024-03-05 13:29:16 -0600

    Add support for tvOS

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SecondFoundation",
-    platforms: [.macOS(.v11), .iOS(.v14)],
+    platforms: [.macOS(.v11), .iOS(.v14), .tvOS(.v14)],
     products: [
         .library(
             name: "SecondFoundation",
