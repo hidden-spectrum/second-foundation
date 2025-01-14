26bcbd39008087b47e15ebcae187a845983f39ed
Andrew Theis <andrew@hiddenspectrum.io>
2025-01-13 22:31:15 -0600

    Add Address protocol with common formatting

---
+++ Sources/SecondFoundation/Protocols/Address.swift
@@ -0,0 +1,85 @@
+//
+//  Copyright © 2025 Hidden Spectrum, LLC.
+//
+
+import CoreLocation
+import Foundation
+
+
+public protocol Address {
+    
+    /// Street
+    var thoroughfare: String? { get }
+    
+    /// Unit
+    var subPremise: String? { get }
+    
+    /// Neighborhood
+    var subLocality: String? { get }
+    
+    /// City
+    var locality: String? { get }
+    
+    /// County
+    var subAdministrativeArea: String? { get }
+    
+    /// City
+    var administrativeArea: String? { get }
+    
+    var postalCode: String? { get }
+    
+    var country: String? { get }
+}
+
+public extension Address {
+    
+    // MARK: Defaults
+    
+    var thoroughfare: String? { nil }
+    var subPremise: String? { nil }
+    var subLocality: String? { nil }
+    var locality: String? { nil }
+    var subAdministrativeArea: String? { nil }
+    var administrativeArea: String? { nil }
+    var postalCode: String? { nil }
+    var country: String? { nil }
+}
+
+public extension Address {
+    
+    // MARK: Utility
+    
+    var formattedAddress: String? {
+        [streetWithSubPremise, cityStateZip]
+            .compactMap{ $0 }
+            .filter { !$0.isEmpty }
+            .joined(separator: "\n")
+            .trimmedNullIfEmpty
+    }
+    
+    var mappableAddress: String? {
+        let fullAddress = [thoroughfare, cityStateZip]
+            .compactMap{ $0 }
+            .joined(separator: ", ")
+        return fullAddress.trimmedNullIfEmpty
+    }
+    
+    var streetWithSubPremise: String? {
+        let street = [thoroughfare, subPremise]
+            .compactMap { $0 }
+            .joined(separator: " ")
+        return street.trimmedNullIfEmpty
+    }
+    
+    var cityState: String? {
+        [locality, administrativeArea]
+            .compactMap { $0?.trimmedNullIfEmpty }
+            .joined(separator: ", ")
+    }
+    
+    var cityStateZip: String? {
+        [cityState, postalCode]
+            .compactMap { $0?.trimmedNullIfEmpty }
+            .joined(separator: " ")
+    }
+}
